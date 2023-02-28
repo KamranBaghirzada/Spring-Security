41473c44a2a1876dcd05e241d950a78bf717fe6d
BagirzadaK <kamran.baghirzada@aist.group>
2023-02-28 21:40:01 +0400

    Adding JwtToken Filter

---
+++ jwt/src/main/java/spring/security/jwt/auth/JwtTokenFilter.java
@@ -0,0 +1,51 @@
+package spring.security.jwt.auth;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.WebAuthenticationDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Service;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+@Service
+@RequiredArgsConstructor
+public class JwtTokenFilter extends OncePerRequestFilter {
+    private final TokenManager tokenManager;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request,
+                                    HttpServletResponse response,
+                                    FilterChain filterChain) throws ServletException, IOException {
+
+        String authHolder = request.getHeader("Authorization");
+        String token = null;
+        String userName = null;
+        if (authHolder != null && authHolder.contains("Bearer")) {
+            token = authHolder.substring(7);
+            try {
+                userName = tokenManager.getUserFromToken(token);
+            } catch (Exception e) {
+                e.getMessage();
+                System.out.println("Could not find user (name) with given token");
+            }
+        }
+        if (token != null && userName != null &&
+                SecurityContextHolder.getContext().getAuthentication() == null) {
+            if (tokenManager.tokenValidate(token)) {
+                UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken
+                        = new UsernamePasswordAuthenticationToken(userName, null, new ArrayList<>());
+                usernamePasswordAuthenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                SecurityContextHolder.getContext().setAuthentication(usernamePasswordAuthenticationToken);
+            }
+        }
+
+    }
+}
